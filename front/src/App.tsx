16b7e59110c49f3063ced17008ba3b774ef0aa2e
anton <antonrumancev67@gmail.com>
2025-11-21 23:43:42 +0300

    Added NavBar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import DoctorList from "./Components/DoctorList";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<DoctorList />} />
         </Routes>
